b1b5782b4cd2b5badad28552007a325cfe535f61
azerkazimov <648azer@gmail.com>
2024-05-11 14:57:51 +0400

    order

--- src/Components/Basket/Order.jsx
+++ src/Components/Basket/Order.jsx
@@ -3,12 +3,10 @@ import PropTypes from "prop-types";
 function Order({ id, name, description }) {
   return (
     <div className="col-12">
-      <div className="product-categories ">
-        <div className="products-item" id={id}>
-          <div className="products-description">
-            <h4>{name}</h4>
-            <p>{description}</p>
-          </div>
+      <div className="products-item mb-2" id={id}>
+        <div className="products-description text-align-start">
+          <h5>{name}</h5>
+          <span>{description}</span>
         </div>
       </div>
     </div>
