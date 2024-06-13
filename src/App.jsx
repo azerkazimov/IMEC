f918cd351452d540fe7047ccb715a8454b7b3606
azerkazimov <648azer@gmail.com>
2024-06-13 21:16:56 +0400

    Burger menu main page color adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
   return (
     <>
       <div className="App">
-        {/* <Navbar /> */}
         <NavbarNew />
         <Routes>
           <Route path="/" element={<Main />} />
--- src/components/layout/Navbar/NavbarNew.jsx
+++ src/components/layout/Navbar/NavbarNew.jsx
@@ -72,6 +72,12 @@ const NavbarNew = () => {
       : "nav-bar active other-page"
     : "nav-bar";
 
+  const burgerMenuColor = isOpenNav
+    ? "bg-natural"
+    : isMainPage
+    ? "bg-natural"
+    : "bg-inky";
+
   return (
     <div className={navCondition}>
       <div className="container">
@@ -86,7 +92,7 @@ const NavbarNew = () => {
             ref={navRef}
           >
             {data.map((item) => (
-              <NavbarLink key={item.id} item={item} navOpen={isOpenNav}/>
+              <NavbarLink key={item.id} item={item} navOpen={isOpenNav} />
             ))}
           </menu>
           <div className="col-2 col-md-3 btn-store">
@@ -109,9 +115,21 @@ const NavbarNew = () => {
               onClick={toggleNav}
               ref={burgerButtonRef}
             >
-              <span className={isOpenNav ? "click" : "unclick"}></span>
-              <span className={isOpenNav ? "click" : "unclick"}></span>
-              <span className={isOpenNav ? "click" : "unclick"}></span>
+              <span
+                className={`${
+                  isOpenNav ? "click" : "unclick"
+                } ${burgerMenuColor}`}
+              ></span>
+              <span
+                className={`${
+                  isOpenNav ? "click" : "unclick"
+                } ${burgerMenuColor}`}
+              ></span>
+              <span
+                className={`${
+                  isOpenNav ? "click" : "unclick"
+                } ${burgerMenuColor}`}
+              ></span>
             </button>
 
             <BasketTab basketTab={basketTab} closeBasket={closeBasket} />
