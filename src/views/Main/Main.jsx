18fe4ecf8a0d3a9ce50ce3cbdb1821ca62574da2
azerkazimov <648azer@gmail.com>
2024-09-21 11:03:59 +0400

    SEO

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import PartnerSection from "../../components/sliders/PartnerSection/PartnerSection.jsx";
 import Slider from "../../components/sliders/Slider/Slider.jsx";
 import {
@@ -9,6 +10,18 @@ import {
 function Main() {
   return (
     <>
+      <Helmet>
+        <link rel="canonical" href="https://imec.az/" />
+        <meta name="description" content="imec engineering company" />
+        <meta name="keywords" content="imec maintenance " />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://imec.az/" />
+        <meta property="og:title" content="IMEC engineering company" />
+        <meta
+          property="og:image"
+          content="https://imec.az/assets/logo-CHKnKiKJ.png"
+        />
+      </Helmet>
       <Header />
       <Slider />
       <About />
